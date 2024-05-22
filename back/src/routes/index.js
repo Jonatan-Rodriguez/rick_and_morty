c17f0223daa4205a762bb04f966d16ecc5ae7d6b
Jonatan-Rodriguez <jonatarodriguez1998@gmail.com>
2024-05-22 16:04:49 -0300

    Eliminacion de controller viejos

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,7 +1,6 @@
 /* Controller */
 const { getCharById } = require('../controllers/getCharById');
 const { getCharAll } = require('../controllers/getCharAll');
-const { postFav, deleteFav, getFav } = require('../controllers/handleFavorites');
 const { login } = require('../controllers/login');
 const { postFavorite } = require('../controllers/postFavorite');
 const { deleteFavorite } = require('../controllers/deleteFavorite');
@@ -18,12 +17,6 @@ router.get('/character/:id', getCharById);
 router.get('/character', getCharAll);
 
 router.get('/login', login);
-/* 
-router.get('./fav', getFav);
-
-router.post('/fav', postFav);
-
-router.delete('/fav/:id', deleteFav); */
 
 router.post('/favorite', postFavorite);
 
