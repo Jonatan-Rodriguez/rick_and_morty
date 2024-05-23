56565a5a6c83e411790915a514c17fea3eb2281a
Jonatan-Rodriguez <jonatarodriguez1998@gmail.com>
2024-05-23 02:29:24 -0300

    Funcion de busqueda con enter agregado

--- front/src/components/SearchBar/SearchBar.jsx
+++ front/src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,3 @@
-//hooks
 import { useDispatch } from "react-redux";
 import { useState } from "react";
 //action
@@ -6,20 +5,26 @@ import { getChar, SearchName } from "../../redux/action";
 //styled
 import { ContainerSearch } from "./searBar.style";
 //assets
-import search from '../../assets/img/search.svg'
+import search from '../../assets/img/search.svg';
 
 const SearchBar = () => {
 
    const [name, setName] = useState('');
    const dispatch = useDispatch();
 
-   let handleChange = (event) => {
+   const handleChange = (event) => {
       setName(event.target.value);
    }
 
    const searchChar = () => {
       dispatch(SearchName(name));
       dispatch(getChar(name, '1'));
+   }
+
+   const handleKeyDown = (event) => {
+      if (event.key === 'Enter') {
+         searchChar();
+      }
    }
 
    const btnRemove = () => {
@@ -31,13 +36,22 @@ const SearchBar = () => {
    return (
       <ContainerSearch>
          <div className="input">
-            <input type="text" className="search__input" placeholder="Buscar" onChange={handleChange} value={name} />
+            <input
+               type="text"
+               className="search__input"
+               placeholder="Buscar"
+               onChange={handleChange}
+               onKeyDown={handleKeyDown}
+               value={name}
+            />
          </div>
          <div className="buttons">
-            {name.length > 0 && <button className="clean__button" onClick={() => { btnRemove() }}>
-               X
-            </button>}
-            <button className="search__button" onClick={() => { searchChar() }}>
+            {name.length > 0 && (
+               <button className="clean__button" onClick={btnRemove}>
+                  X
+               </button>
+            )}
+            <button className="search__button" onClick={searchChar}>
                <img src={search} alt="search" className="search__icon" />
             </button>
          </div>
