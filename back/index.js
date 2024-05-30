318a959f01f8e283f98c212365173ee0cf863b45
Jonatan-Rodriguez <jonatarodriguez1998@gmail.com>
2024-05-30 03:23:42 -0300

    cambio de PORT en index

--- back/index.js
+++ back/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const server = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const { conn } = require('./db');
 
 const { router } = require('./src/routes/index');
