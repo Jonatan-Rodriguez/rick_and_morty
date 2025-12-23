2cb93b56f5444638de7bf039d4c223002c55367c
Jonatan Rodriguez <jonatarodriguez1998@gmail.com>
2025-12-23 14:49:16 -0300

    cambio de estructura

--- back/db.js
+++ back/db.js
@@ -1,39 +1,54 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_URL, DB_USER, DB_PASSWORD, DB_HOST, DB_BDD, DB_PORT } = process.env;
-const FavoriteModel = require('./src/models/Favorite');
-const UserModel = require('./src/models/User');
+const fs = require('fs');
+const path = require('path');
+const {
+  DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_BDD, DB_URL
+} = process.env;
 
-/* const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_BDD}`,
-   { logging: false, native: false }
-); */
-
-const sequelize = new Sequelize(
-   `${DB_URL}`,
-   {
+// Configuración dinámica
+const sequelize = DB_URL
+  ? new Sequelize(DB_URL, {
       logging: false,
       native: false,
       dialectOptions: {
-         ssl: {
-            require: true,
-            rejectUnauthorized: false // Solo si estás usando certificados autofirmados
-         }
+        ssl: {
+          require: true,
+          rejectUnauthorized: false // Necesario para Render
+        }
       }
-   }
-);
+    })
+  : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_BDD}`, {
+      logging: false,
+      native: false,
+    });
 
-FavoriteModel(sequelize);
-UserModel(sequelize);
+const basename = path.basename(__filename);
+const modelDefiners = [];
 
+// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+fs.readdirSync(path.join(__dirname, '/src/models'))
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, '/src/models', file)));
+  });
+
+// Injectamos la conexion (sequelize) a todos los modelos
+modelDefiners.forEach(model => model(sequelize));
+// Capitalizamos los nombres de los modelos ie: product => Product
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+// En sequelize.models están todos los modelos importados como propiedades
+// Para relacionarlos hacemos un destructuring
 const { User, Favorite } = sequelize.models;
-/* suspendo las relaciones por el momento
 
-User.belongsToMany(Favorite, { through: "user_favorite" });
-Favorite.belongsToMany(User, { through: "user_favorite" });
- */
+// Relaciones
+// User.belongsToMany(Favorite, { through: "user_favorite" });
+// Favorite.belongsToMany(User, { through: "user_favorite" });
+
 module.exports = {
-   User,
-   Favorite,
-   conn: sequelize,
+  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
