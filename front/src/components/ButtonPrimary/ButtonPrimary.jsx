e28d93428dc5ed589c7d12f4c0ea5ce5ab5d42de
Jonatan Rodriguez <jonatarodriguez1998@gmail.com>
2026-01-15 19:52:32 -0300

    Limpieza de componentes

--- front/src/components/ButtonPrimary/ButtonPrimary.jsx
+++
@@ -1,9 +0,0 @@
-import { ButtonPrimaryStyled } from "./buttonPrimary.styled";
-
-const ButtonPrimary = ({name}) => {
-    return(
-        <ButtonPrimaryStyled>{name}</ButtonPrimaryStyled>
-    )
-}
-
-export default ButtonPrimary;
